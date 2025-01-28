78e43b9ee87737beb8e1b5eaba9dcd180a95991b
season <sionwer4@naver.com>
2025-01-28 17:32:36 +0900

    feat: Add maximum time setting to blocking message

--- service/block.js
+++ service/block.js
@@ -1,18 +1,50 @@
 export function checkBlockTimeCondition(spentSecond, maximumUsageSecond) {
     if (spentSecond === maximumUsageSecond) {
-        blockYoutube();
+        blockYoutube(maximumUsageSecond);
         return true;
     }
     return false;
 }
 
-export function blockYoutube() {
+export function blockYoutube(maximumUsageSecond) {
     chrome.tabs.query({ url: "*://*.youtube.com/*" }, (tabs) => {
+        const hours = Math.floor(maximumUsageSecond / 3600);
+        const minutes = Math.floor((maximumUsageSecond % 3600) / 60);
+        let maximumUsageTimeSetting;
+        if (hours > 0 && minutes > 0) {
+            maximumUsageTimeSetting = `${hours} hour(s) and ${minutes} minute(s)`;
+        } else if (hours > 0) {
+            maximumUsageTimeSetting = `${hours} hour(s)`;
+        } else if (minutes > 0) {
+            maximumUsageTimeSetting = `${minutes} minute(s)`;
+        }
         tabs.forEach((tab) => {
             chrome.scripting.executeScript({
                 target: { tabId: tab.id },
-                files: ["manipulation/blockOverlay.js"],
+                args: [maximumUsageTimeSetting],
+                func: injectBlockOverlay,
             });
         });
     });
+}
+
+
+function injectBlockOverlay(maximumUsageTimeSetting) {
+    const blocker = document.createElement("div");
+    blocker.id = "youtube-usage-blocker";
+    blocker.innerText = `You have reached your maximum YouTube usage time: ${maximumUsageTimeSetting}`;
+    blocker.style.position = "fixed";
+    blocker.style.width = "100%";
+    blocker.style.height = "100%";
+    blocker.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+    blocker.style.zIndex = "9999";
+    blocker.style.color = "white";
+    blocker.style.fontSize = "30px";
+    blocker.style.display = "flex";
+    blocker.style.justifyContent = "center";
+    blocker.style.alignItems = "center";
+
+    if (!document.getElementById("youtube-usage-blocker")) {
+        document.body.appendChild(blocker);
+    }
 }
